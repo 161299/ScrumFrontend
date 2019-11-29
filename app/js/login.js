866b64b504d65fe6fcab962eeb3fd2106d364fb0
CristhianLaurente <frontend.crstn@gmail.com>
2019-11-29 11:22:09 -0500

    commit 1

--- app/js/login.js
+++ app/js/login.js
@@ -1,10 +1,77 @@
 let formRegistro = document.getElementById("formRegistro");
-let btnRegistro = document.getElementById("btnRegistro")
-let formLogin = document.getElementById("formLogin")
+let btnRegistro = document.getElementById("btnRegistro");
+let formLogin = document.getElementById("formLogin");
+let inputEmail = document.getElementById("inputEmail");
+let inputPassword = document.getElementById("inputPassword");
+let cargando =  document.getElementById("carga")
+let iniciarsesion = document.getElementById("iniciarSesion")
 
-btnRegistro.onclick= ()=>{
-    console.log('se hizo click');
-    formRegistro.removeAttribute('hidden')
-    btnRegistro.setAttribute('hidden','hidden')
-    formLogin.setAttribute('hidden','hidden')
+let login = async () => {
+  let objUsuario = {
+    username: inputEmail.value,
+    password: inputPassword.value
+  };
+  let url = `http://127.0.0.1:5000/usuario/login`;
+  console.log(objUsuario)
+  let reponse = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify(objUsuario)
+  });
+  let objJson = await reponse.json();
+  return objJson;
+};
+
+let traerPersona = async (id) => { 
+  let url = `http://127.0.0.1:5000/persona/${id}`;  
+  let reponse = await fetch(url)
+  let objJson = await reponse.json();
+  console.log(objJson)
+  return objJson;
+};
+
+function parseJwt (token) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+  let json = JSON.parse(jsonPayload)
+  id=json.identity
+  console.log(id)
+  traerPersona(id).then((data)=>{
+    if (data.nombre) {
+      console.log("persona")
+    }else{
+      console.log("agencia")
+    }
+    localStorage.setItem("nombre ",data.nombre+" "+data.apellido)
+    console.log(data)
+  })
+
+};
+
+iniciarsesion.onclick=()=>{
+  cargando.style.display = "block"  
+  login().then(data =>{              
+    if(data.status_code){          
+        cargando.style.display = "none"
+    }else{
+        console.log("bienvenido")
+        cargando.style.display = "none"
+        localStorage.setItem("token",data.access_token)       
+        parseJwt(data.access_token) 
+        
+        //location.href="./normal.html"
+      
+    }}).catch(()=>{
+    console.log("sadsa")
+})
+  
 }
+
+
+
+
---
+++ app/js/normal.js
@@ -0,0 +1,48 @@
+
+    token = localStorage.getItem("token")
+    console.log(token)
+    
+    function parseJwt (token) {
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+        let json = JSON.parse(jsonPayload)
+        console.log(JSON.parse(jsonPayload))
+        id=json.identity
+        console.log(id)
+    };
+    parseJwt(token)
+    
+    let obtenerEstudiante = async (id) => {
+        let url = `http://127.0.0.1:5000/cuenta/${id}`;
+        let reponse = await fetch(url);
+        let objJson = await reponse.json();
+        return objJson;
+    }
+
+    
+    let tabla_body =  document.getElementById("tbody")
+
+
+    let listar = () =>{
+        obtenerEstudiante(id).then(r=>{
+            console.log(r)
+
+            r.forEach(cuenta => {
+            let tr = document.createElement("tr")
+            tr.innerHTML= `<td>${cuenta.numero}</td><td>${cuenta.cue_saldo}</td><td>${cuenta.moneda}</td><td><button onclick="ir(${cuenta.id})">Ir</button></td>` 
+            tabla_body.appendChild(tr)   
+            });
+
+        })
+    }
+
+    listar()
+
+    let ir = (id) =>{
+        console.log(id)
+        localStorage.setItem("idcuenta",id)
+        location.href="./menu.html"
+    }
